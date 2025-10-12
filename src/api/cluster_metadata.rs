8b7fbf71f24e8e2c1100a98933b90c945547f8ad
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2025-10-11 17:13:45 -0700

    feat(Produce): respond for topic and partition

--- src/api/cluster_metadata.rs
+++ src/api/cluster_metadata.rs
@@ -25,17 +25,8 @@ impl RecordBatches {
         &self.batches
     }
 
-    pub fn raw_batch_for_topic(&self, topic_id: &Uuid, partition_id: u32) -> Result<Option<Bytes>> {
-        let topic_name = self.batches.iter().find_map(|b| {
-            b.records.iter().find_map(|r| {
-                if let RecordValue::Topic(topic) = &r.value {
-                    if topic.topic_id == *topic_id {
-                        return Some(topic.topic_name.clone().0.unwrap_or_default());
-                    }
-                }
-                None
-            })
-        });
+    pub fn raw_batch_for_topic(&self, topic_id: &Uuid, partition_id: i32) -> Result<Option<Bytes>> {
+        let topic_name = self.find_topic_name(topic_id);
         if topic_name.as_deref().unwrap_or("").is_empty() {
             return Ok(None);
         }
@@ -46,6 +37,46 @@ impl RecordBatches {
         );
         let file_bytes = std::fs::read(file)?;
         Ok(Some(Bytes::from(file_bytes)))
+    }
+
+    pub fn find_topic_id(&self, topic_name: &str) -> Option<Uuid> {
+        self.batches
+            .iter()
+            .flat_map(|batch| &batch.records)
+            .find_map(|record| {
+                if let RecordValue::Topic(topic) = &record.value {
+                    (topic.topic_name.0.as_deref() == Some(topic_name))
+                        .then(|| topic.topic_id.clone())
+                } else {
+                    None
+                }
+            })
+    }
+
+    pub fn validate_partition(&self, topic_id: &Uuid, partition_index: i32) -> bool {
+        self.batches().iter().any(|batch| {
+            batch.records.iter().any(|record| {
+                if let RecordValue::Partition(partition) = &record.value {
+                    partition.topic_id == *topic_id && partition.partition_id == partition_index
+                } else {
+                    false
+                }
+            })
+        })
+    }
+
+    fn find_topic_name(&self, topic_id: &Uuid) -> Option<String> {
+        self.batches
+            .iter()
+            .flat_map(|batch| &batch.records)
+            .find_map(|record| {
+                if let RecordValue::Topic(topic) = &record.value {
+                    (topic.topic_id == *topic_id)
+                        .then(|| topic.topic_name.0.clone().unwrap_or_default())
+                } else {
+                    None
+                }
+            })
     }
 }
 
@@ -144,7 +175,7 @@ impl Deserialize<Self> for Record {
         };
 
         let value_length = i64::deserialize(src);
-        let value = RecordValue::from_bytes(src);
+        let value = RecordValue::deserialize(src);
         let headers = CompactArray::<Header>::deserialize(src);
 
         Self {
@@ -181,15 +212,15 @@ pub struct TopicValue {
 }
 
 pub struct PartitionValue {
-    pub partition_id: u32,
+    pub partition_id: i32,
     pub topic_id: Uuid,
-    pub replicas: Vec<u32>,
-    pub in_sync_replicas: Vec<u32>,
-    pub removing_replicas: Vec<u32>,
-    pub adding_replicas: Vec<u32>,
-    pub leader_id: u32,
-    pub leader_epoch: u32,
-    partition_epoch: u32,
+    pub replicas: Vec<i32>,
+    pub in_sync_replicas: Vec<i32>,
+    pub removing_replicas: Vec<i32>,
+    pub adding_replicas: Vec<i32>,
+    pub leader_id: i32,
+    pub leader_epoch: i32,
+    partition_epoch: i32,
     directories: Vec<Uuid>,
 }
 
@@ -206,9 +237,10 @@ enum RecordType {
     FeatureLevel = 12,
 }
 
-impl RecordValue {
-    fn from_bytes(src: &mut Bytes) -> Self {
-        assert_eq!(src.get_u8(), 1); // frame_version
+impl Deserialize<Self> for RecordValue {
+    fn deserialize(src: &mut Bytes) -> Self {
+        let frame_version = src.get_u8();
+        assert_eq!(frame_version, 1);
         let record_type = RecordType::try_from(src.get_u8()).unwrap();
         let version = src.get_u8();
 
@@ -222,15 +254,15 @@ impl RecordValue {
             }
             RecordType::Partition => {
                 assert_eq!(version, 1);
-                let partition_id = src.get_u32();
+                let partition_id = src.get_i32();
                 let topic_id = Uuid::deserialize(src);
-                let replicas = CompactArray::<u32>::deserialize(src);
-                let in_sync_replicas = CompactArray::<u32>::deserialize(src);
-                let removing_replicas = CompactArray::<u32>::deserialize(src);
-                let adding_replicas = CompactArray::<u32>::deserialize(src);
-                let leader_id = src.get_u32();
-                let leader_epoch = src.get_u32();
-                let partition_epoch = src.get_u32();
+                let replicas = CompactArray::<i32>::deserialize(src);
+                let in_sync_replicas = CompactArray::<i32>::deserialize(src);
+                let removing_replicas = CompactArray::<i32>::deserialize(src);
+                let adding_replicas = CompactArray::<i32>::deserialize(src);
+                let leader_id = src.get_i32();
+                let leader_epoch = src.get_i32();
+                let partition_epoch = src.get_i32();
                 let directories = CompactArray::<Uuid>::deserialize(src);
                 RecordValue::Partition(PartitionValue {
                     partition_id,
